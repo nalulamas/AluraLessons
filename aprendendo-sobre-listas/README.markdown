01e04a2521dbf65c7a0b8d9d26bac0c81cbf9a38
Paulo <analu.lamas@yahoo.com.br>
2022-03-10 19:56:37 -0300

    @nalulamas

---
+++ aprendendo-sobre-listas/README.markdown
@@ -0,0 +1,5 @@
+# Aprendendo sobre Listas
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/nalulamas/pen/BamXXgj](https://codepen.io/nalulamas/pen/BamXXgj).
+
+
---
+++ aprendendo-sobre-listas/src/script.js
@@ -0,0 +1,12 @@
+var listaFilmes = ["Yesterday", "A chegada", "Escola de Rock", "Harry Potter"];
+listaFilmes.push("A origem");
+
+for (var indice = 0; indice < listaFilmes.length; indice++) {
+  document.write("<p>" + indice + "</p>");
+  document.write("<p>" + listaFilmes[indice] + "</p>");
+}
+//var filme1 = "Yesterday";
+//var filme2 = "A chegada";
+//var filme3 = "Escola de Rock";
+
+//experimentar o h1, strong....
